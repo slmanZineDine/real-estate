02e8ac8748ebbe612c98a2e21fdb4305c9d20cc2
Slman <slman.zinedine1@gmail.com>
2022-04-04 21:20:52 +0300

    upload files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Responsive Nav-Bar
+let mobile = document.querySelector(".mobile");
+let links = document.querySelector(".links");
+
+mobile.onclick = function () {
+    links.classList.toggle("responsive")
+}
