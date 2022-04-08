c37684ef405b7383f86d934898ccebecb65c6dbf
Slman <slman.zinedine1@gmail.com>
2022-04-08 19:45:23 +0300

    update pages

--- js/main.js
+++ js/main.js
@@ -3,5 +3,21 @@ let mobile = document.querySelector(".mobile");
 let links = document.querySelector(".links");
 
 mobile.onclick = function () {
-    links.classList.toggle("responsive")
+    links.classList.toggle("responsive");
+}
+
+// Scroll To Top
+let btn = document.querySelector(".btn-scroll");
+if(btn) {
+    function showBtn() {
+        if(window.innerHeight < window.scrollY )
+            btn.style.display = "block";
+        else
+        btn.style.display = "none";
+    }
+    function toTop() {
+        window.scrollTo({top: 0, left:0, behavior: "smooth"});
+    }
+    window.onscroll = showBtn;
+    btn.addEventListener("click", toTop)
 }
